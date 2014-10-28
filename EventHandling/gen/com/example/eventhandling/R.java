337146893c733f12560f6affa9f52781e8c5f11a
Joanne Caroll <joannecaroll@UNIT001.(none)>
2014-10-28 14:41:04 +0800

    Button that displays either textview or toast that displays (hello world)

--- EventHandling/gen/com/example/eventhandling/R.java
+++ EventHandling/gen/com/example/eventhandling/R.java
@@ -1091,7 +1091,6 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1105,6 +1104,7 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int hello_world=0x7f05003c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1132,7 +1132,9 @@ containing a value of this type.
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
+        public static final int showText=0x7f05003d;
         public static final int showTitle=0x7f050006;
+        public static final int showToast=0x7f05003e;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
@@ -1195,9 +1197,6 @@ containing a value of this type.
         public static final int activity_main=0x7f030018;
         public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
-    public static final class menu {
-        public static final int main=0x7f0c0000;
-    }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
@@ -1239,9 +1238,10 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int show=0x7f0a000f;
+        public static final int showToast=0x7f0a0010;
     }
     public static final class style {
         /** 
--- EventHandling/src/com/example/eventhandling/MainActivity.java
+++ EventHandling/src/com/example/eventhandling/MainActivity.java
@@ -1,36 +1,53 @@
 package com.example.eventhandling;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
+	TextView label;
+	Button showButton;
+	Button showToast;
+	int i=0;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        
+        label = (TextView)findViewById(R.id.hello_world);
+		label.setVisibility(View.INVISIBLE);
+        showButton = (Button)findViewById(R.id.showText);
+        showButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (i==0)
+				{
+					label.setVisibility(View.VISIBLE);
+					i=1;
+				}
+				else if(i==1)
+				{
+					label.setVisibility(View.INVISIBLE);
+					i=0;
+				}
+			}
+		});
+        
+        showToast = (Button)findViewById(R.id.showToast);
+        showToast.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, R.string.hello_world, Toast.LENGTH_SHORT).show();
+			}
+		});
+        	
         }
-        return super.onOptionsItemSelected(item);
-    }
+        
+
 }
